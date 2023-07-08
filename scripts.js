447d66b9dc69d2ae4765dcdb0f3383adc3e9f86a
Adithya Ajay <adithyanajay@gmail.com>
2023-07-08 21:23:32 +0530

    project completed

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
-document.getElementById("hamburger").addEventListener("click", openNav)
 
+// code to open navbar in mobile mode when the hamburger menu is clicked
+document.getElementById("hamburger").addEventListener("click", openNav)
 function openNav() {
    document.getElementById("navlinks").classList.add("active")
 }
+
+// code to close navbar in mobile mode when the close button is clicked
 document.getElementById("close").addEventListener("click", closeNav)
 function closeNav() {
     document.getElementById("navlinks").classList.remove("active")
